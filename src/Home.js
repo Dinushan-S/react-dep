ca386e84bb325e63eeeda82bb13d5ac1f89b55a1
Dinushan <selvandinu18@gmail.com>
2023-05-23 09:34:14 +0530

    final app

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h3 className='d-flex justify-content-center m-3'>
+                    This is a Home Page
+                </h3>
+            </div>
+        )
+    }
+}
